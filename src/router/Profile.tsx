8f467dbe993ebffd6dbfffebf635766d45c42055
신희성 <vvwv3092@gmail.com>
2023-07-28 08:49:45 +0900

    Fixed displayName space

--- src/router/Profile.tsx
+++ src/router/Profile.tsx
@@ -157,7 +157,11 @@ export default function Profile({
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(displayName);
+
     if (!displayName?.length) return;
+    if (!/^[^\s]+$/.test(displayName))
+      return alert("이름에 공백이 들어갈 수 없습니다!");
     if (displayName?.length > 8) return alert("8글자 이내로 입력해주세요!");
     if (!displayName) return alert("변경할 이름을 입력해주세요!");
     if (userObj?.displayName ?? userObj?.email?.split("@")[0] !== displayName) {
